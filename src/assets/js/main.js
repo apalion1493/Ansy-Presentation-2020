4f922c26b07f8abb969dc4cfa125abd4cc81dfd9
Alexandr Petrovich <apalion97@gmail.com>
2020-08-04 11:15:56 +0300

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import jQuery from 'jquery';
+import 'bootstrap/dist/js/bootstrap.min';
+import 'popper.js/dist/popper.min';
+import 'wowjs/dist/wow.min';
+import 'slick-carousel/slick/slick.min';
+import 'scrollmagic/scrollmagic/minified/ScrollMagic.min';
+
+$('[data-toggle="popover"]').popover();
+$('[data-toggle="tooltip"]').tooltip();
